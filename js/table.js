7b553d55dc0f91e0775f1696d2d7dbdaf37748d3
Unknown <catarina.pedreira@tecnico.ulisboa.pt>
2018-10-09 00:34:39 +0100

    Added class table

---
+++ js/table.js
@@ -0,0 +1,35 @@
+class Table extends GraphicalObject {
+	
+	constructor(x,y,z){
+		super();
+
+		material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe:true});
+
+		addTableTop(this,x,y,z);
+		addTableSupports(this, x - 7, y - 5, z - 4.3); 
+		addTableSupports(this, x + 7, y - 5, z + 4.6);
+		addTableSupports(this, x + 7, y - 5, z - 4.6);
+		addTableSupports(this, x - 7, y - 5, z + 4.6);
+
+	}
+
+	function addTableTop(obj,x,y,z) {	
+		'use scrict';
+
+		geometry = new THREE.CubeGeometry(15,1,10);
+		material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true});
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set(x,y - 1,z);
+		this.add(mesh);
+	}
+
+	function addTableSupports(obj,x,y,z) {
+		'use scrict';
+
+		geometry = new THREE.CylinderGeometry(0.5,0.5,7);
+		material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set(x,y,z);
+		this.add(mesh);
+	}
+}
