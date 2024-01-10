ce83023f354f22a9f38228d08365129e8e1e40eb
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2024-01-10 05:45:47 +0000

    finished edu and jobs slides

--- js/custom.js
+++ js/custom.js
@@ -112,14 +112,16 @@ const dataPT = {
             'escola': 'Escola Profissional',
             'curso': 'Técnico Informática e Gestão',
             'campi': 'N/A',
+            'img': 'assets/img/gallery-75-10.jpg',
             'local': 'Salvaterra de Magos',
-            'descricao': 'yo Bozo'
+            'descricao': '<p>Sup</p><p>Bozo</p>'
         },
         { 
             'id': 'EDISTOE',
             'escola': 'Instituto Superior Tecnico',
             'curso': 'Mestrado Engenharia Informática e Computadores',
             'campi': 'Taguspark',
+            'img': 'assets/img/gallery-75-11.jpg',
             'local': 'Oeiras',
             'descricao': 'yo Bozo'
         },
@@ -130,6 +132,7 @@ const dataPT = {
             'empresa': 'DHL Supply Chain',
             'cargo': 'Técnico de Informática',
             'duracao': '3 anos',
+            'img': 'assets/img/gallery-75-12.jpg',
             'descricao': 'sup Bozo',
         },
         {
@@ -137,6 +140,7 @@ const dataPT = {
             'empresa': 'Adecco - Arvato',
             'cargo': 'Call Center - Clientes alemães',
             'duracao': '6 meses',
+            'img': 'assets/img/gallery-75-13.jpg',
             'descricao': 'sup Bozo'
         },
         {
@@ -144,12 +148,14 @@ const dataPT = {
             'empresa': 'Semente',
             'cargo': 'Full Stack Developer',
             'duracao': '1 ano',
+            'img': 'assets/img/gallery-75-14.jpg',
             'descricao': 'sup Bozo'
         },
         {
             'id': 'EXMORPHOME',
             'empresa': 'Bold - Morphis',
             'cargo': 'Consultor de Informática',
+            'img': 'assets/img/gallery-75-15.jpg',
             'descricao': 'sup Bozo'
         },
     ],
@@ -358,3 +364,59 @@ function buildTecnologiaList(listid) {
     }
     return tec;
 }
+
+function setupSlide2(){
+    function createLi(obj){
+        let li = document.createElement('li');
+        let img = document.createElement('img');
+        li.appendChild(img);
+        li.setAttribute("class", "col-3-12 col-tablet-1-3 col-phablet-1-2 col-phone-1-1 ae-7 fadeIn");
+        img.setAttribute("src", obj['img']);
+        img.setAttribute("class", 'wide rounded margin-bottom-1');
+        img.setAttribute("alt", 'Image');
+        return li;
+
+    }
+    function createpopLi(obj){
+        let li = document.createElement('li');
+        let objText = document.createElement('span');
+        let img = document.createElement('img');
+        let divider = document.createElement('div');
+        divider.setAttribute("class", 'fix-12-12');
+        divider.appendChild(img)
+        li.appendChild(divider);
+        let cloneDivider = divider.cloneNode();
+        cloneDivider.appendChild(objText)
+        li.appendChild(cloneDivider);
+        
+        img.setAttribute("src", obj['img']);
+        img.setAttribute("class", 'rounded');
+        img.setAttribute("alt", 'Image');
+        
+        objText.innerHTML = obj['descricao'];
+
+        return li;
+    }
+
+    let ul =  document.getElementById('slider2UL');
+    let ulpop = document.getElementById('slider2popUL');
+    let ulCar =  document.getElementById('slider2ULCar');
+    let ulpopCar = document.getElementById('slider2popULCar');
+    let edu = dataPT['educacao'];
+    let career = dataPT['experiencia'];
+
+    for(step of edu){
+        ul.appendChild(createLi(step));
+        ulpop.appendChild(createpopLi(step));
+    }
+
+    for(step of career){
+        ulCar.appendChild(createLi(step));
+        ulpopCar.appendChild(createpopLi(step));
+    }
+}
+
+//main
+$(document).ready(() => {
+    setupSlide2();
+});
