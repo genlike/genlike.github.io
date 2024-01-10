7e95e3396ee4c53af15e5b872bb75279d856207e
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2024-01-10 02:07:36 +0000

    finished dataset

---
+++ js/custom.js
@@ -0,0 +1,360 @@
+const dataPT = {
+    'projectos': [
+        {
+            'id': 'PROJPAP',
+            'local': 'EDUEPSM',
+            'areas': ['ARBACK', 'ARFRONT', 'ARDB', 'ARDESK'],
+            'tecnologias': ['TECVB','TECMSSQL', 'TECPHP'],
+            'descricao': 'Fiz a PAP',
+        },
+        {
+            'id': 'PROJTMS',
+            'local': 'EXDHLAL',
+            'areas': ['ARDESK', 'ARDB'],
+            'tecnologias': ['TECVB', 'TECXML', 'TECDBINF'],
+            'descricao': 'Fiz desenvolvimentos no TMS Interfaces',
+        },
+        {
+            'id': 'PROJINFOLG',
+            'local': 'EXDHLAL',
+            'areas': ['ARDESK', 'ARDB'],
+            'tecnologias': ['TECVB6','TECDBINF'],
+            'descricao': 'Fiz desenvolvimentos no Infolog Extender',
+        },
+        {
+            'id': 'PROJREDPRAIRIE',
+            'local': 'EXDHLAL',
+            'areas': ['ARDB'],
+            'tecnologias': ['TECORAC'],
+            'descricao': 'Fiz reports no RedPrairie',
+        },
+        {
+            'id': 'PROJPHC',
+            'local': 'EXDHLAL',
+            'areas': ['ARDB'],
+            'tecnologias': ['TECMSSQL'],
+            'descricao': 'Fiz reports no PHC',
+        },
+        {
+            'id': 'PROJXBOX',
+            'local': 'EXADECXBOX',
+            'areas': [''],
+            'tecnologias': [''],
+            'descricao': 'Yo je parle german',
+        },
+        {
+            'id': 'PROJWS',
+            'local': 'EDISTOE',
+            'areas': [''],
+            'tecnologias': ['TECPYTH', 'TECASSEMB'],
+            'descricao': 'Formação de alunos nas linguagens de Python e Assembly',
+        },
+        {
+            'id': 'PROJBANCS',
+            'local': 'EDISTOE',
+            'areas': ['ARBACK', 'ARFRONT', 'ARAG'],
+            'tecnologias': ['TECPYTH', 'TECDJANGO', 'TECJIR', 'TECGCLOUD', 'TECGIT'],
+            'descricao': 'Yo Startup failed but tried',
+        },
+        {
+            'id': 'PROJNEIIST',
+            'local': 'EDISTOE',
+            'areas': [],
+            'tecnologias': [],
+            'descricao': 'Fui Vice Presi, im salty',
+        },
+        {
+            'id': 'PROJTESE',
+            'local': 'EDISTOE',
+            'areas': ['ARBACK', 'ARFRONT', 'ARCLOUD', 'ARCONT', 'ARDB'],
+            'tecnologias': ['TECPYTH', 'TECDJANGO', 'TECJIR', 'TECGCLOUD', 'TECGIT'],
+            'descricao': 'Yo Startup failed but tried',
+        },
+        {
+            'id': 'PROJWCREP',
+            'local': 'EXSEMLIS',
+            'areas': ['ARBACK', 'ARFRONT'],
+            'tecnologias': ['TECCSHARP', 'TECNETCORE','TECANDR', 'TECHTCSS', 'TECGIT'],
+            'descricao': 'Build WC Reporter',
+        },
+        {
+            'id': 'PROJCSIKEA',
+            'local': 'EXSEMLIS',
+            'areas': ['ARIOT'],
+            'tecnologias': ['TECRASP', 'TECPYTH'],
+            'descricao': 'Yo casa smart',
+        },
+        {
+            'id': 'PROJCONTPEP',
+            'local': 'EXSEMLIS',
+            'areas': ['ARIOT', 'ARFRONT', 'ARBACK'],
+            'tecnologias': ['TECARM', 'TECC', 'TECGIT'],
+            'descricao': 'Yo contar pessoas com uma camara que nao funciona',
+        },
+        {
+            'id': 'PROJFILRAPID',
+            'local': 'EXSEMLIS',
+            'areas': ['ARIOT', 'ARFRONT', 'ARBACK'],
+            'tecnologias': ['TECESP86','TECNETCORE', 'TECC', 'TECGIT'],
+            'descricao': 'Yo reiventei a fila unica do continet, cause swag&rizz',
+        },
+        {
+            'id': 'PROJTRLGCS',
+            'local': 'EXMORPHOME',
+            'areas': ['ARFRONT', 'ARBACK'],
+            'tecnologias': ['TECCSHARP','TECNETCORE', 'TECORAC', 'TECGIT'],
+            'descricao': 'Transformar Oracle Forms para C#',
+        },
+    ],
+    'educacao': [
+        {
+            'id': 'EDUEPSM',
+            'escola': 'Escola Profissional',
+            'curso': 'Técnico Informática e Gestão',
+            'campi': 'N/A',
+            'local': 'Salvaterra de Magos',
+            'descricao': 'yo Bozo'
+        },
+        { 
+            'id': 'EDISTOE',
+            'escola': 'Instituto Superior Tecnico',
+            'curso': 'Mestrado Engenharia Informática e Computadores',
+            'campi': 'Taguspark',
+            'local': 'Oeiras',
+            'descricao': 'yo Bozo'
+        },
+    ],
+    'experiencia': [
+        {
+            'id': 'EXDHLAL',
+            'empresa': 'DHL Supply Chain',
+            'cargo': 'Técnico de Informática',
+            'duracao': '3 anos',
+            'descricao': 'sup Bozo',
+        },
+        {
+            'id': 'EXADECXBOX',
+            'empresa': 'Adecco - Arvato',
+            'cargo': 'Call Center - Clientes alemães',
+            'duracao': '6 meses',
+            'descricao': 'sup Bozo'
+        },
+        {
+            'id': 'EXSEMLIS',
+            'empresa': 'Semente',
+            'cargo': 'Full Stack Developer',
+            'duracao': '1 ano',
+            'descricao': 'sup Bozo'
+        },
+        {
+            'id': 'EXMORPHOME',
+            'empresa': 'Bold - Morphis',
+            'cargo': 'Consultor de Informática',
+            'descricao': 'sup Bozo'
+        },
+    ],
+    'areas': [
+        {
+            'id':'ARBACK',
+            'nome':'Backend',
+        },
+        {
+            'id':'ARDESK',
+            'nome':'Desktop Application',
+        },
+        {
+            'id':'ARFRONT',
+            'nome':'Frontend',
+        },
+        {
+            'id':'ARCLOUD',
+            'nome':'Cloud',
+        },
+        {
+            'id':'ARCONT',
+            'nome':'Container',
+        },
+        {
+            'id':'ARIOT',
+            'nome':'Internet of Things',
+        },
+        {
+            'id':'ARDB',
+            'nome':'Databases',
+        },
+        {
+            'id':'ARAG',
+            'nome':'Agile Tools',
+        },
+    ],
+    'tecnologias': [
+        {
+            'id': 'TECPYTH',
+            'nome': 'Python'
+
+        },
+        {
+            'id': 'TECASSEMB',
+            'nome': 'Assembly Pepe-16'
+
+        },
+        {
+            'id': 'TECGIT',
+            'nome': 'Git'
+
+        },
+        {
+            'id': 'TECC',
+            'nome': 'C'
+
+        },
+        {
+            'id': 'TECJAVA',
+            'nome': 'Java'
+
+        },
+        {
+            'id': 'TECCSHARP',
+            'nome': 'C#'
+
+        },
+        {
+            'id': 'TECVB',
+            'nome': 'VB.NET'
+
+        },
+        {
+            'id': 'TECJSCRIPT',
+            'nome': 'Javascript'
+
+        },
+        {
+            'id': 'TECTSCRIPT',
+            'nome': 'Typescript'
+
+        },
+        {
+            'id': 'TECNODE',
+            'nome': 'Node.JS'
+
+        },
+        {
+            'id': 'TECNETCORE',
+            'nome': '.NET Core'
+
+        },
+        {
+            'id': 'TECDJANGO',
+            'nome': 'Django'
+
+        },
+        {
+            'id': 'TECDOCKER',
+            'nome': 'Docker'
+
+        },
+        {
+            'id': 'TECAWS',
+            'nome': 'AWS'
+
+        },
+        {
+            'id': 'TECGCLOUD',
+            'nome': 'Google Cloud'
+
+        },
+        {
+            'id': 'TECRASP',
+            'nome': 'Raspberry Pi'
+
+        },
+        {
+            'id': 'TECESP86',
+            'nome': 'ESP86'
+
+        },
+        {
+            'id': 'TECANDR',
+            'nome': 'Android'
+
+        },
+        {
+            'id': 'TECMSSQL',
+            'nome': 'MSSQL'
+
+        },
+        {
+            'id': 'TECORAC',
+            'nome': 'PL/SQL Oracle'
+        },
+        {
+            'id': 'TECPHP',
+            'nome': 'PHP'
+        },
+        {
+            'id': 'TECVB6',
+            'nome': 'VB6'
+        },
+        {
+            'id': 'TECDBINF',
+            'nome': 'Informix Database'
+        },
+        {
+            'id': 'TECXML',
+            'nome': 'XML'
+        },
+        {
+            'id': 'TECJIR',
+            'nome': 'Jira'
+        },
+        {
+            'id': 'TECHTCSS',
+            'nome': 'HTML/CSS'
+        },
+        {
+            'id': 'TECARM',
+            'nome': 'ARM System'
+        },
+    ],
+};
+
+function getDataEducacaoById(id){
+   return dataPT['educacao'].filter(ed => ed.id === id)[0];
+}
+
+function getDataExperienciaById(id){
+    return dataPT['experiencia'].filter(ex => ex.id === id)[0];
+}
+
+function getDataAreaById(id){
+    return dataPT['areas'].filter(ar => ar.id === id)[0];
+}
+
+function getDataTecnologiaById(id){
+    return dataPT['tecnologias'].filter(te => te.id === id)[0];
+}
+
+function getDataProjectoById(id){
+    let proj = Object.assign({}, dataPT['projectos'].filter(pr => pr.id === id)[0]);
+    proj['local'] = getDataEducacaoById(proj['local']) ?? getDataExperienciaById(proj['local']);
+    proj['areas'] = buildAreaList(proj['areas']);
+    proj['tecnologias'] = buildTecnologiaList(proj['tecnologias']);
+
+    return proj;
+}
+
+function buildAreaList(listid) {
+    let areas = [];
+    for(id of listid) {
+        areas.push(getDataAreaById(id));
+    }
+    return areas;
+}
+
+function buildTecnologiaList(listid) {
+    let tec = [];
+    for(id of listid) {
+        tec.push(getDataTecnologiaById(id));
+    }
+    return tec;
+}
